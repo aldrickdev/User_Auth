3ac68541cfebfe4a378675478f23b85ba531ab9a
aldrickdev <aldrickdev@gmail.com>
2021-04-29 19:08:39 -0400

    Added base routes

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,2 +1,35 @@
+from fastapi import FastAPI
 
-print("hello")
+# class App:
+#   def __init__(self, app):
+#     print('Created App')
+
+# Database
+users = []
+
+app: FastAPI = FastAPI()
+@app.get('/')
+def index():
+  return {'Hello': 'World'}
+
+@app.post('/login')
+def login():
+  return {'Page': 'Login'}
+
+@app.post('/register')
+def register():
+  return {'Page': 'Register'}
+
+@app.get('/dashboard')
+def dashboard():
+  return {'Page': 'Dashboard'}
+
+@app.delete('/delete')
+def delete():
+  return {'Page': 'Delete'}
+
+def main():
+  print('Starting App')
+
+if __name__ == '__main__':
+  main()
